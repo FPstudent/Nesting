1d9efcc816903b1bb0a3c4f2be0ab8fa643dd000
Federica Pecci <federica.pecci2@studio.unibo.it>
2020-04-24 19:29:06 +0200

    delete old project, modify git ignore

--- Nesting_2/IConfigurationIO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nesting_2
-{
-	public interface IConfigurationIO
-	{
-        Configuration ReadAllData(string fileName);
-
-        void WriteAllData(ref Configuration configuration, string fileName);
-	}
-}
--- Nesting_3/IConfigurationIO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Nesting_3
-{
-	public interface IConfigurationIO
-	{
-        Configuration ReadAllData(string fileName);
-
-        void WriteAllData(ref Configuration configuration, string fileName);
-	}
-}
